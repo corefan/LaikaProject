21552adb9974c9127400010b23ddf731806fa623
swkwon <swkwon04@gmail.com>
2014-03-28 16:19:31 +0900

    adding MessagePack

---
+++ Laika/Serialize/MessagePack/MessagePackUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MsgPack.Serialization;
+
+namespace Laika.Serialize.MessagePack
+{
+    public class MessagePackUtility
+    {
+        public static byte[] Serialize<T>(T instance)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = MessagePackSerializer.Create<T>();
+                serializer.Pack(stream, instance);
+                stream.Position = 0;
+                return stream.ToArray();
+            }
+        }
+
+        public static T Deserialize<T>(byte[] serializedData)
+        {
+            using (var stream = new System.IO.MemoryStream(serializedData))
+            {
+                var serializer = MessagePackSerializer.Create<T>();
+                return serializer.Unpack(stream);
+            }
+        }
+    }
+}
