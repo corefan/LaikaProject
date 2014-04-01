4f114e64f6f1fb1ab32a8e5a6992b84f8335bd4a
swkwon <swkwon04@gmail.com>
2014-04-01 11:18:22 +0900

    network/mysql utility

---
+++ Laika/Database/MySqlUtility.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Laika.Database
+{
+    public class MySqlUtility
+    {
+        public static bool IsSafeString(string parameter)
+        {
+            if (string.IsNullOrEmpty(parameter))
+                return false;
+
+            return _safeStringChecker.IsMatch(parameter);
+        }
+        private static Regex _safeStringChecker = new Regex(@"^[\w\s\.\?!@#%&=~:/,_-]*$", RegexOptions.Compiled);
+
+        public static bool isSafeStringLoose( string parameter )
+        {
+            if(string.IsNullOrEmpty(parameter))
+                return false;
+
+            return _safeStringLooseChecker.IsMatch( parameter );
+        }
+        private static Regex _safeStringLooseChecker = new Regex( @"^[\w\s\.\$\^\{\[\(\|\)\*\+\?!@#%&=~_,<>:/-]*$", RegexOptions.Compiled );
+
+
+        /// <summary>
+        /// 쿼리에 들어가는 인자가 SQL Injection 공격에 안전한 한 단어 문자인지 체크합니다.
+        /// </summary>
+        /// <param name="queryParameter">쿼리에 들어가는 문자열 인자</param>
+        /// <returns>해당 인자가 안전한 한 단어 문자인지의 여부</returns>
+        public static bool isSafeOneWordString( string parameter )
+        {
+            if (string.IsNullOrEmpty(parameter))
+                return false;
+
+            return _safeOneWordStringChecker.IsMatch( parameter );
+        }
+        private static Regex _safeOneWordStringChecker = new Regex( @"^\w*$", RegexOptions.Compiled );
+    }
+}
---
+++ Laika/Network/NetworkUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace Laika.Network
+{
+    public class NetworkUtility
+    {
+        public static int IPToInteger(string intAddress)
+        {
+            return BitConverter.ToInt32(IPAddress.Parse(intAddress).GetAddressBytes(), 0);
+        }
+
+        public static string IntegerToIP(int intAddress)
+        {
+            return new IPAddress(BitConverter.GetBytes(intAddress)).ToString();
+        }
+    }
+}
